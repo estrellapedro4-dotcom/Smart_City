70e16f972d1b5bc823f82c41d0ceff2a2f7cc2f9
Pedro Estrella <estrellapedro4@gmail.com>
2026-03-17 10:00:54 +0000

    Versão estável inicial do projeto

---
+++ Smart_City/src/test/java/com/example/Smart_City/SmartCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Smart_City;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
